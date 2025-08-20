9072868f684f29e94d9a83de36e9f2fcc50b0517
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-21 01:48:41 +0530

    Update SpeechRecognition.py

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -2,7 +2,7 @@
 import speech_recognition as sr 
                      
 def transcribe_audio(file_path):             
-    recognizer = sr.Recognizer()
+    recognizer = sr.Recognizer() 
     with sr.AudioFile(file_path) as source: 
         audio =   recognizer.record(source)
     try:
