5fba36efd6b2e3b9c088a4e8f3eaca6eb8661138
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-21 01:49:45 +0530

    Update Frontend UI.py

--- Frontend UI.py
+++ Frontend UI.py
@@ -6,7 +6,7 @@ from summarize_text import  generate_summary
 st.title("EchoLogic – Voice to Docs")    
     
 audio_file = st.file_uploader("Upload Audio File", type=["wav", "mp3"])
-if audio_file:
+if audio_file: 
     with open("temp_audio.wav", "wb") as f:
         f.write(audio_file.read())
     transcript = transcribe_audio("temp_audio.wav")
