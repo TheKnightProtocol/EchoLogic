7478d0081536155777b660c884ca7c11d97859a7
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-09-08 23:48:18 +0530

    Update SpeechRecognition.py

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 def transcribe_audio(file_path):             
     recognizer = sr.Recognizer() 
     with sr.AudioFile(file_path) as source:    
-        audio =    recognizer.record(source)
+        audio =    recognizer.record(source) 
     try: 
         text =    recognizer.recognize_google(audio)
         return text  
