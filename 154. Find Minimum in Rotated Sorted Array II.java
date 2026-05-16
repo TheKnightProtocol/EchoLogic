25a9035efb0ea3c251b7a7e9ca0ff92cd1827619
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-05-16 13:51:10 +0530

    Update and rename Frontend UI.py to 154. Find Minimum in Rotated Sorted Array II.java
    Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
    
    [4,5,6,7,0,1,4] if it was rotated 4 times.
    [0,1,4,4,5,6,7] if it was rotated 7 times.
    Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
    
    Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
    
    You must decrease the overall operation steps as much as possible.
    
     
    
    Example 1:
    
    Input: nums = [1,3,5]
    Output: 1
    Example 2:
    
    Input: nums = [2,2,2,0,1]
    Output: 0
     
    
    Constraints:
    
    n == nums.length
    1 <= n <= 5000
    -5000 <= nums[i] <= 5000
    nums is sorted and rotated between 1 and n times.

--- 154. Find Minimum in Rotated Sorted Array II.java
+++ 154. Find Minimum in Rotated Sorted Array II.java
@@ -1,4 +1,19 @@
-# app.py                                                                                                                                                                                                                    
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+}# app.py                                                                                                                                                                                                                    
 import streamlit as st                                        
 from transcribe_audio import    transcribe_audio                         
 from summarize_text import            generate_summary               
